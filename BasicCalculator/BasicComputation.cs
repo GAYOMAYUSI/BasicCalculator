6e30c9691e2f31dd2f0076ca28e25eaa821ba54f
Gayoma <Gayoma@GAYOMAYUSI>
2025-10-27 19:34:50 +0800

    Initial commit to program in frmBasicCalculator.Cs and BasicComputation.cs

--- BasicCalculator/BasicComputation.cs
+++ BasicCalculator/BasicComputation.cs
@@ -8,6 +8,29 @@ namespace BasicCalculator
 {
     internal class BasicComputation
     { 
-     
+        public static float Addition(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public static float Subtraction(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public static float Multiplication(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public static float Division(float num1, float num2)
+        {
+            if (num2 == 0)
+            {
+                return 0;
+            }
+
+            return num1 / num2;
+        }
     }
 }
--- BasicCalculator/FrmBasicCalculator.cs
+++ BasicCalculator/FrmBasicCalculator.cs
@@ -20,14 +20,44 @@ namespace BasicCalculator
 
         private void FrmBasicCalculator_Load(object sender, EventArgs e)
         {
-           
+            var operations = new List<string> { "+", "-", "*", "/" };
+
+            foreach (var operation in operations) cbOperation.Items.Add(operation);
 
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-         
+            try
+            {
+                var num1 = Convert.ToInt32(tbInput1.Text);
+                var num2 = Convert.ToInt32(tbInput2.Text);
+                var operation = cbOperation.SelectedItem.ToString();
+                var result = "None";
+
+               switch (operation)
+{
+    case "+":
+        result = Convert.ToString(BasicComputation.Addition(num1, num2));
+        break;
+    case "-":
+        result = Convert.ToString(BasicComputation.Subtraction(num1, num2));
+        break;
+    case "*":
+        result = Convert.ToString(BasicComputation.Division(num1, num2));
+        break;
+    case "/":
+        result = Convert.ToString(BasicComputation.Multiplication(num1, num2));
+        break;
+}
+
+                MessageBox.Show($"Result: {result}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
         }
     }
 }
